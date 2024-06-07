f2241b5f2d8ce553c2e519f14ba2cb4880b22c4a
Armita <its.fake.one.da@gmail.com>
2024-06-07 12:30:24 +0330

    all done

--- src/main/java/org/example/pinterestdemoworkshop/HelloApplication.java
+++ src/main/java/org/example/pinterestdemoworkshop/HelloApplication.java
@@ -1,8 +1,11 @@
 package org.example.pinterestdemoworkshop;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,11 +14,15 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 877.0, 591.0);
+        stage.setTitle("Pinterest-demo");
         stage.setScene(scene);
+        Image icon = new Image(getClass().getResourceAsStream("/Screenshot2024-06-06172349.png"));
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/org/example/pinterestdemoworkshop/HelloController.java
+++ src/main/java/org/example/pinterestdemoworkshop/HelloController.java
@@ -1,6 +1,7 @@
 package org.example.pinterestdemoworkshop;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -10,5 +11,47 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    Button saveBTN;
+
+    @FXML
+    public void save() {
+        String x = saveBTN.getText();
+        if (x.equals("Save")) {
+            saveBTN.setText("Saved");
+        }
+        else {
+            saveBTN.setText("Save");
+        }
+    }
+
+    @FXML
+    Button followBTN;
+
+    @FXML
+    public void follow() {
+        String x = followBTN.getText();
+        if (x.equals("Follow")) {
+            followBTN.setText("Unfollow");
+        }
+        else {
+            followBTN.setText("Follow");
+        }
+    }
+
+    @FXML
+    Button heartBTN;
+
+    @FXML
+    private void heart() {
+        String currentColor = heartBTN.getStyle();
+        if (currentColor.contains("black")) {
+            heartBTN.setStyle("-fx-text-fill: red;");
+        }
+        else {
+            heartBTN.setStyle("-fx-text-fill: black;");
+        }
     }
 }
